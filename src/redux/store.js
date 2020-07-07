9ccaa6509a4374f3da99e0ffa121c7d28cba4da4
Anthony Bernard <tony@anthonys-mbp.home>
2020-07-07 18:48:42 -0400

    added local storage

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,17 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware } from "redux";
 //import { composeWithDevTools } from 'redux-devtools-extension';
-import thunk from 'redux-thunk';
-import rootReducer from './rootReducer';
+import thunk from "redux-thunk";
+import rootReducer from "./rootReducer";
 
-const initialState = {};
+const persistedState = localStorage.getItem("reduxState")
+  ? JSON.parse(localStorage.getItem("reduxState"))
+  : {};
 
-const store = createStore(rootReducer, initialState, applyMiddleware(thunk))
+const store = createStore(rootReducer, persistedState, applyMiddleware(thunk));
+
+store.subscribe(() => {
+  localStorage.setItem("reduxState", JSON.stringify(store.getState()));
+});
 
 /* composeWithDevTools(applyMiddleware(thunk) */
 
